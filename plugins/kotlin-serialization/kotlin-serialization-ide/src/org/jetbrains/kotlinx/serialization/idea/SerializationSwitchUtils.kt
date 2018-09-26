264105d2dbfb22821812b1f9f1be86391b5fb211
Leonid Startsev <sandwwraith@gmail.com>
2018-09-26 19:43:33 +0300

    Improve logic behind finding ModuleInfo and move its common part to idea-core

--- plugins/kotlin-serialization/kotlin-serialization-ide/src/org/jetbrains/kotlinx/serialization/idea/SerializationSwitchUtils.kt
+++ plugins/kotlin-serialization/kotlin-serialization-ide/src/org/jetbrains/kotlinx/serialization/idea/SerializationSwitchUtils.kt
@@ -5,19 +5,14 @@
 
 package org.jetbrains.kotlinx.serialization.idea
 
-import com.intellij.openapi.module.Module
 import org.jetbrains.kotlin.analyzer.ModuleInfo
 import org.jetbrains.kotlin.descriptors.ClassDescriptor
-import org.jetbrains.kotlin.idea.caches.project.ModuleSourceInfo
-import org.jetbrains.kotlin.idea.caches.project.PlatformModuleInfo
-import org.jetbrains.kotlin.idea.caches.project.getModuleInfo
+import org.jetbrains.kotlin.idea.core.unwrapModuleSourceInfo
 import org.jetbrains.kotlin.idea.facet.KotlinFacet
-import org.jetbrains.kotlin.js.resolve.diagnostics.findPsi
-import org.jetbrains.kotlin.psi.KtElement
+import org.jetbrains.kotlin.resolve.descriptorUtil.module
 
 fun <T> getIfEnabledOn(clazz: ClassDescriptor, body: () -> T): T? {
-    val sourceElement: KtElement = (clazz.findPsi() as? KtElement) ?: return null
-    val module = sourceElement.getModuleInfo().findModule() ?: return null
+    val module = clazz.module.getCapability(ModuleInfo.Capability)?.unwrapModuleSourceInfo()?.module ?: return null
     val facet = KotlinFacet.get(module) ?: return null
     val pluginClasspath = facet.configuration.settings.compilerArguments?.pluginClasspaths ?: return null
     if (pluginClasspath.none { it == KotlinSerializationImportHandler.PLUGIN_JPS_JAR }) return null
@@ -25,11 +20,3 @@ fun <T> getIfEnabledOn(clazz: ClassDescriptor, body: () -> T): T? {
 }
 
 fun runIfEnabledOn(clazz: ClassDescriptor, body: () -> Unit) { getIfEnabledOn<Unit>(clazz, body) }
-
-private fun ModuleInfo.findModule(): Module? {
-    return when (this) {
-        is ModuleSourceInfo -> this
-        is PlatformModuleInfo -> this.platformModule
-        else -> null
-    }?.module
-}
